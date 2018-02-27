4d33068295f52268c8cd74c7a084f254e10f4b2d
jmilch <36661409+jmilch@users.noreply.github.com>
2018-02-27 09:48:14 +0100

    Update Digispark_USBMIDI_Controller.ino

--- Digispark_USBMIDI_Controller.ino
+++ Digispark_USBMIDI_Controller.ino
@@ -1,6 +1,16 @@
 #include <DigiMIDI.h>
+#include <Bounce2.h>
 
 DigiMIDIDevice midi;
+#define BUTTON_PIN_0 0
+#define BUTTON_PIN_1 1
+#define BUTTON_PIN_2 2
+#define BUTTON_PIN_5 5
+
+Bounce debouncer0 = Bounce(); 
+Bounce debouncer1 = Bounce(); 
+Bounce debouncer2 = Bounce(); 
+Bounce debouncer5 = Bounce(); 
 
 int const numDigPinsCC = 4; // number of digital pins to send CC
 int currentDigcc[numDigPinsCC]; //+1 for double switch
@@ -8,6 +18,7 @@ int digitalpincc[] = {
    0,1,2,5 // which digital pins to use for sending CC
 };
 int digInputcc[numDigPinsCC];
+int value[] = {0, 0, 0, 0};
 
 
 
@@ -22,7 +33,14 @@ void setup() {
     pinMode(digitalpincc[i], INPUT);
     digitalWrite(digitalpincc[i],HIGH);
 }
- 
+  debouncer0.attach(BUTTON_PIN_0);
+  debouncer0.interval(5); // interval in ms
+  debouncer1.attach(BUTTON_PIN_1);
+  debouncer1.interval(5); // interval in ms
+  debouncer2.attach(BUTTON_PIN_2);
+  debouncer2.interval(5); // interval in ms
+  debouncer5.attach(BUTTON_PIN_5);
+  debouncer5.interval(5); // interval in ms
 
   Serial.begin(38400);
 
@@ -30,14 +48,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-// digital pins sending CC
+  // Update the Bounce instances :
+  debouncer0.update();
+  debouncer1.update();
+  debouncer2.update();
+  debouncer5.update();
+
+  // Get the updated value :
+  int value[0] = debouncer0.read();
+  int value[1] = debouncer1.read();
+  int value[2] = debouncer2.read();
+  int value[3] = debouncer5.read();
+   
+   
+   // digital pins sending CC
 
   for (int i = 0; i < numDigPinsCC; i++) {
-    if (digitalRead(digitalpincc[i]) == 1 && currentDigcc[i] == 0) {
+    if (value[i]) == 1 && currentDigcc[i] == 0) {
       midi.sendControlChange(i+50, 127, channel); 
       currentDigcc[i] = 1;
     }  
-    if (digitalRead(digitalpincc[i]) == 0  && currentDigcc[i] == 1) {
+    if (value[i]) == 0  && currentDigcc[i] == 1) {
       midi.sendControlChange(i+50, 0, channel);
       currentDigcc[i] = 0;
     }  
